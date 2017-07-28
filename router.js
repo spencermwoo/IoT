e376b6e40e9f10753d7d80a41dd74dbb0858d251
Spencer <spencer.woo@liferay.com>
2017-07-28 15:22:37 -0700

    log status print

--- router.js
+++ router.js
@@ -10,9 +10,29 @@ var casper = require('casper').create({
   }
 });
 
+// print out all the messages in the headless browser context
+casper.on('remote.message', function(msg) {
+  this.echo('\n\nMessage Caught : ' + msg);
+});
+
+// print out all the messages in the headless browser context
+casper.on("page.error", function(msg, trace) {
+  this.echo("\n\nPage Error : " + msg, "ERROR");
+});
+
 casper.start(RESET_URL, function() {
   //if already logged in
+  this.echo('\n\nCurrent URL : ' + this.getCurrentUrl() + '\n\n');
   this.click('input[class=submitBtn]');
+
+  //otherwise auth via input
+});
+
+casper.thenEvaluate(function(){
+  console.log("=====================");
+  console.log("Page Title : " + document.title);
+  console.log("Page Body : " + document.body);
+  console.log("=====================");
 });
 
 casper.run();
